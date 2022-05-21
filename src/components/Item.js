434f48f2fe392f84bde5a7fd88402e5576f9de6c
oriolbv <oriolbv@gmail.com>
2022-05-21 21:32:13 +0200

    Item component to functional

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,16 +2,17 @@ import React, {PureComponent} from "react";
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
 
-class Item extends PureComponent{
-	render(){
-		const { id, poster_path, title } = this.props.data;
-		return (<Link className="grid-item-link" to={`/${id}`}>
+function Item(props) {
+	const { id, poster_path, title } = props.data;
+	
+	return (
+		<Link className="grid-item-link" to={`/${id}`}>
 			<div className="grid-item" >
 				<img className="preview-image" src={`https://image.tmdb.org/t/p/original${poster_path}`}/>
 				<div className="preview-title">{title}</div>
 			</div>
-		</Link>);
-	}
+		</Link>
+	);
 }
 
 Item.propTypes = {
